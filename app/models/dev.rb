9930ee2d8beb7da92417c7329dac629998e79531
Yael Amir <yaelamir1@gmail.com>
2016-02-25 13:10:44 -0800

    change default user picture

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -11,7 +11,7 @@ class Dev < ActiveRecord::Base
       medium:   "300x300>",
       thumb:    "100x100>"
     },
-    default_url: "/images/:style/missing.png"
+    default_url: "/images/default-user.png"
   )
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
   # <%= image_tag.each do |pic| %>
